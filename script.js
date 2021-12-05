c7ee11682b2b12091457cd5723b9e26ddba94229
Milkessa Gebi <milkessagebi@gmail.com>
2021-12-06 02:03:34 +0300

    u[

--- script.js
+++ script.js
@@ -6,4 +6,5 @@ console.log(document.querySelector('.message').textContent);
 document.querySelector('.number').textContent = 13;
 document.querySelector('.score').textContent = 10;
 
+document.querySelector('.guess').value = 24;
 console.log(document.querySelector('.guess').value);

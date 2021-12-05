513fcc862a20dc8244f92786c1b3159676a50a7c
Milkessa Gebi <milkessagebi@gmail.com>
2021-12-06 01:58:18 +0300

    js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 'use strict';
+
+console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent = '🎊 CORRECT NUMBER';
+console.log(document.querySelector('.message').textContent);
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
+
+console.log(document.querySelector('.guess').value);

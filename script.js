d243d4e46a6850dd351a0bff9143fe06dc0cb7fc
Milkessa Gebi <milkessagebi@gmail.com>
2021-12-08 13:15:48 +0300

    .

--- script.js
+++ script.js
@@ -9,11 +9,10 @@
 // document.querySelector('.guess').value = 24;
 // console.log(document.querySelector('.guess').value);
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 
-document.querySelector('.number').textContent = secretNumber;
-
+document.querySelector('.number').textContent = '?';
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
 
@@ -25,7 +24,7 @@ document.querySelector('.check').addEventListener('click', function () {
     //When Players Win
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '🎊✔️ Correct Number';
-
+    document.querySelector('.number').textContent = secretNumber;
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
 
@@ -51,4 +50,15 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.score').textContent = 0;
     }
   }
+});
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+
+  document.querySelector('.score').textContent = score;
+  let secretNumber = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector('.message').textContent = 'Start guessing.....';
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
 });

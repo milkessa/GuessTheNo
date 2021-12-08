f18d0bb9e7fa40d6529a2c76e023cc39e2c38d78
Milkessa Gebi <milkessagebi@gmail.com>
2021-12-08 12:46:27 +0300

    .

--- script.js
+++ script.js
@@ -9,6 +9,11 @@
 // document.querySelector('.guess').value = 24;
 // console.log(document.querySelector('.guess').value);
 
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+
+document.querySelector('.number').textContent = secretNumber;
+
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
 
@@ -16,5 +21,24 @@ document.querySelector('.check').addEventListener('click', function () {
 
   if (!guess) {
     document.querySelector('.message').textContent = '⛔ No Number';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '🎊✔️ Correct Number';
+  } else if (guess > secretNumber) {
+    if (score > 0) {
+      document.querySelector('.message').textContent = 'Too High';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'Sorry You Have Lost';
+      document.querySelector('.score').textContent = 0;
+    }
+  } else if (guess < secretNumber) {
+    if (score < 0) {
+      document.querySelector('.message').textContent = 'Sorry You Have Lost';
+    } else {
+      document.querySelector('.message').textContent = 'Too Low';
+      score--;
+      document.querySelector('.score').textContent = score;
+    }
   }
 });
